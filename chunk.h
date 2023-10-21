b24d5c1563800cb3907724797e112fe00664ca65
Adam The Canadian <adam.jarrett.clare@gmail.com>
2023-10-21 08:14:13 -0600

    Up to 15, end of 14

--- chunk.h
+++ chunk.h
@@ -11,6 +11,7 @@
 // Operation Code == OpCode
 // Add, subtraction, look up, etc
 typedef enum {
+  OP_CONSTANT,
   // Return from the current function
   OP_RETURN,
 } OpCode;
@@ -23,12 +24,14 @@ typedef struct {
   int capacity;
   // Series of single byte instructions
   uint8_t *code;
+  // The line number for each byte in the bytecode
+  int *lines;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 int addConstant(Chunk *chunk, Value value);
 
 #endif
--- debug.h
+++ debug.h
@@ -4,6 +4,8 @@
 #include "chunk.h"
 
 void disassembleChunk(Chunk* chunk, const char *name);
+// We return the number of bytes/offset to the next instruction
+// this is done since different OpCodes can have different sizes
 int disassembleInstruction(Chunk* chunk, int offset);
 
 #endif
